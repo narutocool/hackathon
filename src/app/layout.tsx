01b2b0a99f066c5930961c1a4504d1cac0b75733
Vinh LTT <vinh.ltt@clv.local>
2023-12-17 13:49:52 +0700

    123

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <EventContext.Provider
         value={{ event, setEvent, isShowNavigation, setShowNavigation }}
       >
