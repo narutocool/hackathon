f0460c381639d79fc1708e668121acccceb2ed63
Vinh LTT <vinh.ltt@clv.local>
2023-12-17 12:47:28 +0700

    prepare data

--- dummyData.ts
+++ dummyData.ts
@@ -1,12 +1,39 @@
 export const DUMMY_DATA = [
   {
+    eventName: "Past event",
+    eventCode: "PAST",
+    eventStartTime: "2023-12-16T00:00:00",
+    eventFinishTime: "2023-12-16T00:00:00",
+    eventDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+    eventLocation: "Floor 5th - Project",
+    eventType: "Company event",
+    eventDuration: 48,
+    eventTimeline: [
+      {
+        title: "Title4",
+        location: "Location",
+        startTime: "2023-12-17T18:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+      {
+        title: "Title5",
+        location: "Location",
+        startTime: "2023-12-17T19:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+    ],
+    eventRecap: ["recap1", "recap2"],
+    guests: ["abc@gmail.com", "def@gmail.com"],
+  },
+  {
     eventName: "Hackathon 2023",
     eventCode: "HKTN",
     eventStartTime: "2023-12-17T00:00:00",
     eventFinishTime: "2023-12-18T00:00:00",
     eventDescription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
-    eventLocation: "text",
+    eventLocation: "Floor 5th - Project",
     eventType: "Company event",
     eventDuration: 48,
     eventTimeline: [
@@ -25,7 +52,7 @@ export const DUMMY_DATA = [
       {
         title: "Title3",
         location: "Location",
-        startTime: "2023-12-17T16:00:00",
+        startTime: "2023-12-17T12:00:00",
         endTime: "2021-01-01T00:00:00",
       },
       {
@@ -40,6 +67,85 @@ export const DUMMY_DATA = [
         startTime: "2023-12-17T19:00:00",
         endTime: "2021-01-01T00:00:00",
       },
+    ],
+    eventRecap: ["recap1", "recap2"],
+    guests: ["abc@gmail.com", "def@gmail.com"],
+  },
+  {
+    eventName: "Company workshop and Hackathon GALA 2023",
+    eventCode: "GALA",
+    eventStartTime: "2023-12-20T00:00:00",
+    eventFinishTime: "2023-12-20T00:00:00",
+    eventDescription:
+      "For your kind information, our Company Workshop 2023 is going to take place in December with the GALA Round of Hackathon Festival 2023. We welcome all of your participation to this remarkable event and share memorable moments with us",
+    eventLocation: "Capella Park View",
+    eventType: "Company event",
+    eventDuration: 48,
+    eventTimeline: [
+      {
+        title: "Worksop",
+        location: "Capella Park View",
+        startTime: "2023-12-20T12:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+      {
+        title: "Hackathon round 2",
+        location: "Capella Park View",
+        startTime: "2023-12-20T14:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+      {
+        title: "Title3",
+        location: "Capella Park View",
+        startTime: "2023-12-20T16:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+      {
+        title: "Title4",
+        location: "Capella Park View",
+        startTime: "2023-12-20T18:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+      {
+        title: "Title5",
+        location: "Capella Park View",
+        startTime: "2023-12-20T20:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+    ],
+    eventRecap: ["recap1", "recap2"],
+    guests: ["abc@gmail.com", "def@gmail.com"],
+  },
+  {
+    eventName: "Christmas party",
+    eventCode: "CRPT",
+    eventStartTime: "2023-12-23T13:00:00",
+    eventFinishTime: "2023-12-23T00:00:00",
+    eventDescription:
+      "For your kind information, our Company Workshop 2023 is going to take place in December with the GALA Round of Hackathon Festival 2023. We welcome all of your participation to this remarkable event and share memorable moments with us",
+    eventLocation: "6th Floor",
+    eventType: "Company event",
+    eventDuration: 4,
+    eventTimeline: [
+      {
+        title: "Worksop",
+        location: "Capella Park View",
+        startTime: "2023-12-23T13:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+      {
+        title: "Hackathon round 2",
+        location: "Capella Park View",
+        startTime: "2023-12-23T14:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+      {
+        title: "Title3",
+        location: "Capella Park View",
+        startTime: "2023-12-23T15:00:00",
+        endTime: "2021-01-01T00:00:00",
+      },
+      
     ],
     eventRecap: ["recap1", "recap2"],
     guests: ["abc@gmail.com", "def@gmail.com"],
